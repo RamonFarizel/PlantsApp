e7948db1ea13904395115811824fec439b219941
Ramon Farizel <ramonfarizel@Ramon-Farizel-MacBook-Pro-316A.local>
2021-04-14 10:48:24 -0300

    Creating page bottom

---
+++ XFTeste/ViewModels/DetailsPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace XFTeste.ViewModels
+{
+    public class DetailsPageViewModel : ObservableObject
+    {
+        AsyncCommand _backNavigationCommand;
+        public AsyncCommand BackNavigationCommand => _backNavigationCommand ??
+            (_backNavigationCommand = new AsyncCommand(ExecuteBackNavigationCommand));
+
+        async Task ExecuteBackNavigationCommand() =>
+            await App.Current.MainPage.Navigation.PopAsync();
+
+        public DetailsPageViewModel()
+        {
+
+        }
+    }
+}
