8d61c70d2ed837ee38ed9faa326fcc5c82f17e88
Ramon Farizel <ramonfarizel@Ramon-Farizel-MacBook-Pro-316A.local>
2021-04-14 10:03:52 -0300

    Creating details page

--- XFTeste/MainPageViewModel.cs
+++ XFTeste/MainPageViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+using XFTeste.Views;
 
 namespace XFTeste
 {
@@ -12,9 +15,18 @@ namespace XFTeste
             set => SetProperty(ref _title, value);
         }
 
+        IAsyncCommand _navigateCommand;
+        public IAsyncCommand NavigateCommand => _navigateCommand ??
+            (_navigateCommand = new AsyncCommand(ExecuteNavigateCommand));
+
         public MainPageViewModel()
         {
             Title = "Testing";
+        }
+
+        async Task ExecuteNavigateCommand()
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new DetailsPage());
         }
     }
 }
